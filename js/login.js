70343b29608f55534ac1c5de7f8f2734d0cb2896
XQxiaoqvan <xiaoqvandd@qq.com>
2023-10-14 21:05:58 +0800

    更新 login.js

--- js/login.js
+++ js/login.js
@@ -9,16 +9,18 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         return;
     }
 
-    var hashedPassword = 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3';
+    var hashedPassword = '75f97061c05ef39f090b8af856e32dccf2fef7cb7d6642dab4c611165ea87e44';
 
     if (CryptoJS.SHA256(password).toString() === hashedPassword) {
-        // 登录成功，保存认证信息到LocalStorage
-        localStorage.setItem('xqhtToken', 'xiaoqvanht_renzhengzhuangyong'); // 这里用一个示例认证信息
+        // 登录成功，保存认证信息到cookie
+        var expirationDate = new Date();
+        expirationDate.setDate(expirationDate.getDate() + 7); // 设置过期时间为一周后
+        document.cookie = 'xqhtToken=xiaoqvanht_renzhengzhuangyong; expires=' + expirationDate.toUTCString() + '; SameSite=Strict';
         alert('登录成功！');
         window.location.href = 'https://pan.xiaoqvan.top/admin';
     } else {
         alert('密码错误！');
         // 清除认证信息
-        localStorage.removeItem('xqhtToken');
+        document.cookie = 'xqhtToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; SameSite=Strict';
     }
 });
